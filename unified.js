f1d88168220975f3ad4da0560b586932d0f5dd16
Lukas Schwab <lukas.schwab@gmail.com>
2023-04-13 15:34:33 -0700

    Experiment: appending phonetic alphabet

--- unified.js
+++ unified.js
@@ -1,71 +1,52 @@
-/**
- * INPUT-TO-DATE UTILS.
- */
-
-// NOTE: warning visibility is controlled from the selected parser.
-const warning = document.getElementById('warning');
-
-const unixParser = (n) => {
-  let ms;
-  if (n.length === 10) {
-    // Treat as seconds.
-    ms = 1000 * parseInt(n);
-    warning.style.display = "block";
-  } else {
-    ms = parseInt(n);
-    warning.style.display = "none";
-  }
-  return new Date(ms);
+const NATO = {
+  "a": "Alfa",
+  "b": "Bravo",
+  "c": "Charlie",
+  "d": "Delta",
+  "e": "Echo",
+  "f": "Foxtrot",
+  "g": "Golf",
+  "h": "Hotel",
+  "i": "India",
+  "j": "Juliett",
+  "k": "Kilo",
+  "l": "Lima",
+  "m": "Mike",
+  "n": "November",
+  "o": "Oscar",
+  "p": "Papa",
+  "q": "Quebec",
+  "r": "Romeo",
+  "s": "Sierra",
+  "t": "Tango",
+  "u": "Uniform",
+  "v": "Victor",
+  "w": "Whiskey",
+  "x": "Xray",
+  "y": "Yankee",
+  "z": "Zulu"
 }
 
-const mongoParser = (s) => {
-  const ms = parseInt(s.substring(0, 8), 16) * 1000;
-  return new Date(ms);
-}
+const toPhonetic = (char) => {
+  label = document.createElement("label")
+  label.classList.add("list-group-item-label", "quiet")
+    label.appendChild(document.createTextNode(char === " " ? "[space]" : `${char} as in`))
 
-const isoParser = (s) => new Date(s);
+  item = document.createElement("li")
+  item.classList.add("list-group-item", "list-group-item-action")
+  item.appendChild(label)
 
-var error;
-
-const showErrorIfInvalid = (d) => {
-  if (isNaN(d.getTime())) {
-    error.style.display = "block";
-  } else {
-    error.style.display = "none";
+  if (char === " ") {
+    return item
   }
-}
 
-/**
- * DATE-TO-OUTPUT UTILS.
- */
+  item.appendChild(document.createTextNode(NATO[char.toLowerCase()] ?? char))
 
-const toDefaultOutput = (d) => d;
-
-const toCodeOutput = (d) => `new Date(${d.getTime()})`;
-
-const toReadableOutput = (d) => d.toLocaleDateString(
-  'en-US',
-  {
-    month: 'long',
-    day: 'numeric',
-    year: 'numeric'
-  }
-);
-
-const toISOOutput = (d) => d.toISOString();
-
-const toMongoOutput = (d) => {
-  const ms = d.getTime();
-  const prefix = Math.floor(ms / 1000).toString(16);
-  return prefix + "0000000000000000";
+  return item
 }
 
 const outputsAndGenerators = new Map([
-  [document.getElementById('default-output'), toDefaultOutput],
-  [document.getElementById('code-output'), toCodeOutput],
-  [document.getElementById('readable-output'), toReadableOutput],
-  [document.getElementById('iso-output'), toISOOutput],
-  [document.getElementById('mongo-output'), toMongoOutput],
+  [document.getElementById('default-output'), (text) => text.split("").map(toPhonetic).join("<br>")]
 ]);
 
 /**
@@ -74,95 +55,22 @@ const outputsAndGenerators = new Map([
 
 // Changing the input updates the outputs.
 const input = document.getElementById('input');
+const results = document.getElementById('results')
 
 const setOutputs = () => {
-  const date = inputParser(input.value);
-  showErrorIfInvalid(date);
-  outputsAndGenerators.forEach((generator, output) => {
-    output.innerHTML = isNaN(date.getTime()) ? "Invalid Date" : generator(date);
-  });
+  // Clear and reconstruct children.
+  results.innerHTML = ""
+  input.value.split("").forEach(char => {
+    results.appendChild(toPhonetic(char))
+  })
 }
 
 input.addEventListener('input', setOutputs);
 
 const reset = () => {
-  input.value = dateToValidInput(new Date());
+  input.value = "";
   setOutputs();
 }
-
-// Clicking the reset link sets to the input to the current timestamp.
-Array.from(document.getElementsByClassName("reset-to-now")).forEach(
-  link => link.addEventListener("click", reset)
-);
-
-/**
- * SWITCHING UI.
- */
-
-const dropdown = document.getElementById("somedropdown");
-
-const switchToUnixLink = document.getElementById("switch-to-unix");
-const unixError = document.getElementById("unix-error");
-
-const switchToISOLink = document.getElementById("switch-to-iso");
-const isoError = document.getElementById("iso-error");
-
-const switchToMongoLink = document.getElementById("switch-to-mongo");
-const mongoError = document.getElementById("mongo-error");
-
-// (String | Number) => Date
-var inputParser;
-// (Date) => String | Number
-var dateToValidInput;
-
-function switchToUnix() {
-  dropdown.innerText = "Unix Time";
-  input.type = "number";
-  inputParser = unixParser;
-  // Needs to be a number rather than a string.
-  dateToValidInput = (d) => d.getTime();
-  switchToUnixLink.classList.add("active");
-  switchToISOLink.classList.remove("active");
-  switchToMongoLink.classList.remove("active");
-  error = unixError;
-}
-
-switchToUnixLink.addEventListener('click', () => {
-  switchToUnix();
-  reset();
-});
-
-function switchToISO() {
-  dropdown.innerText = "ISO 8601";
-  input.type = "text"
-  inputParser = isoParser;
-  dateToValidInput = toISOOutput;
-  switchToISOLink.classList.add("active");
-  switchToUnixLink.classList.remove("active");
-  switchToMongoLink.classList.remove("active");
-  error = isoError;
-}
-
-switchToISOLink.addEventListener('click', () => {
-  switchToISO();
-  reset();
-});
-
-function switchToMongo() {
-  dropdown.innerText = "Mongo ID";
-  input.type = "text"
-  inputParser = mongoParser;
-  dateToValidInput = toMongoOutput;
-  switchToMongoLink.classList.add("active");
-  switchToUnixLink.classList.remove("active");
-  switchToISOLink.classList.remove("active");
-  error = mongoError;
-}
-
-switchToMongoLink.addEventListener('click', () => {
-  switchToMongo();
-  reset();
-});
 
 /**
  * INITIALIZATION
@@ -172,21 +80,8 @@ switchToMongoLink.addEventListener('click', () => {
 // the current timestamp.
 const queried = decodeURIComponent(window.location.search.substring(1));
 if (queried && queried.length > 0) {
-  console.log("Initializing with query value", queried)
-  if (queried.match(/^\d+$/g)) {
-    console.log("Switching on query to Unix");
-    switchToUnix();
-  } else if (!isNaN((new Date(queried)).getTime())) {
-    console.log("Switching on query to ISO");
-    switchToISO();
-  } else {
-    console.log("Defaulting on query to Mongo");
-    switchToMongo();
-  }
   input.value = queried;
   setOutputs();
 } else {
-  // Without a query, default to Unix.
-  switchToUnix();
   reset();
 }
